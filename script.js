6337a082c0eabc879764128ce82ae9b998ff0818
GabrielDevJourney <gabspereira178@gmail.com>
2024-08-13 08:20:00 +0100

    refactor done now is time to test all of it

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-//get input fields and buttons
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const pagesInput = document.getElementById("pages");
-const readOrNotInput = document.getElementById("readornot");
-const modalToAddBook = document.getElementById("wrapper");
-const btnCloseModal = document.querySelector(".close-book-form");
-const btnSubmitBook = document.querySelector(".submit-btn");
-
-//GETTING THE WEBPAGE WRAPPER
-const wrapper = document.querySelector(".wrapper");
-
-//MAKE THE ADD BOOK BTN OPEN MODAL
-const btnAddBook = document.querySelector(".btn-add-book");
-
 //function to open modal and blur bcg
 function openModal() {
 	modalToAddBook.style.display = "block";
@@ -114,15 +99,6 @@ class Book {
 		};
 	}
 }
-
-function createNewBookFromInput() {
-	const title = titleInput.value;
-	const author = authorInput.value;
-	const pagesNumber = pagesInput.value;
-	const isRead = readOrNotInput.checked;
-	return new Book(title, author, pagesNumber, isRead);
-}
-
 class Library {
 	constructor() {
 		this._myLibrary = [];
@@ -171,6 +147,7 @@ class Library {
 			(book) => book instanceof Book && !book.isRead
 		).length;
 	}
+
 	getTotalPagesRead() {
 		return this._myLibrary.reduce((total, book) => {
 			if (book instanceof Book && book.isRead) {
@@ -179,256 +156,246 @@ class Library {
 			return total;
 		}, 0);
 	}
+
 	saveLibraryInLocalStorage() {
 		localStorage.setItem("myLibrary", JSON.stringify(this._myLibrary));
 	}
+
 	loadLibraryFromLocalStorage() {
 		const storedLibrary = localStorage.getItem("myLibrary");
 		if (storedLibrary) {
 			const parsedLibrary = JSON.parse(storedLibrary);
-			this._myLibrary = parsedLibrary.map(bookData => new Book(
-                bookData.title,
-                bookData.author,
-                bookData.pages,
-                bookData.isRead,
-                bookData.id
-            ))
-            return true
+			this._myLibrary = parsedLibrary.map(
+				(bookData) =>
+					new Book(
+						bookData.title,
+						bookData.author,
+						bookData.pages,
+						bookData.isRead,
+						bookData.id
+					)
+			);
+			return true;
 		}
-        return false
+		return false;
 	}
 }
+class Ui {
+	constructor(library) {
+		this.library = library;
+		this.bookContainer = document.querySelector(".book-container");
+		//elements already exist in html
+		this.titleInput = document.getElementById("title");
+		this.authorInput = document.getElementById("author");
+		this.pagesInput = document.getElementById("pages");
+		this.readOrNotInput = document.getElementById("readornot");
+		this.modalToAddBook = document.querySelector(".form-wrapper");
+		this.btnCloseModal = document.querySelector(".close-book-form");
+		this.btnSubmitBook = document.querySelector(".submit-btn");
+		this.wrapper = document.querySelector(".wrapper");
+		this.btnAddBook = document.querySelector(".btn-add-book");
+		this.cardsContainer = document.querySelector(".cards-container");
 
-//FUNC TO CLEAR INPUT
-function clearInputAfterSubmit() {
-	titleInput.value = "";
-	authorInput.value = "";
-	pagesInput.value = "";
-	readOrNotInput.checked = false;
-}
-
-//FCUNTION THAT WILL STORE INFO AND CALL THE FCUNTION TO CREATE CARD
-function addNewBook() {
-	const newBook = getBookInfo();
-	myLibrary.push(newBook);
-
-	saveLibraryInLocalStorage();
-	closeModal();
-	createBookCard(newBook);
-	updateStatsWhenCardCreated(newBook);
-	clearInputAfterSubmit();
-}
-
-//CREATE FUCNTION TO BUILD THE CARD
-//declare in the whole scope to be able to access them in other func
-
-function createBookCard(book) {
-	//grab cards container
-	const cardsContainer = document.querySelector(".cards-container");
-	//create elements of card
-	const card = document.createElement("div");
-	const cardInfo = document.createElement("div");
-	const title = document.createElement("h2");
-	const author = document.createElement("p");
-	const pages = document.createElement("p");
-	const readContainer = document.createElement("div");
-	const isReadIcon = document.createElement("img");
-	const noneReadIcon = document.createElement("img");
-
-	//ADD CLASSES TO ELEMENTS
-	card.classList.add("card");
-	cardInfo.classList.add("card-info-enable");
-	title.classList.add("card-header");
-	author.classList.add("card-author");
-	pages.classList.add("card-pages");
-	readContainer.classList.add("read-or-nonread-icons-container");
-	isReadIcon.classList.add("card-icon", "is-read");
-	noneReadIcon.classList.add("card-icon", "none-read");
-
-	//create data id to identify each card
-	card.setAttribute("data-id", book.id);
-
-	//Adding icon sources
-	isReadIcon.src = "assets/readcheck stats.png";
-	isReadIcon.alt = "";
-	noneReadIcon.src = "assets/cancel stats.png";
-	noneReadIcon.alt = "";
-
-	//Tex content from book object
-	title.textContent = book.title;
-	author.textContent = `By ${book.author}`;
-	pages.textContent = `${book.pages} pages`;
-
-	// Determine which icon to display based on isRead property
-	if (book.isRead) {
-		readContainer.appendChild(isReadIcon);
-	} else {
-		readContainer.appendChild(noneReadIcon);
+		//inicialize event listeners
+		this.initEventListeners();
 	}
 
-	//APPEDING ELEMENTS
-	cardsContainer.appendChild(card);
-	card.appendChild(cardInfo);
-	cardInfo.appendChild(title);
-	cardInfo.appendChild(author);
-	cardInfo.appendChild(pages);
-	cardInfo.appendChild(readContainer);
+	initEventListeners() {
+		this.btnAddBook.addEventListener("click", () => this.openModal());
+		this.btnCloseModal.addEventListener("click", () => this.closeModal());
+		this.btnSubmitBook.addEventListener("click", () => this.addNewBook());
+	}
 
-	//refer to the fucntion and append to the main card
-	const btnsShowWhenHover = createHoverBtns(
+	createBookCard(book) {
+		const card = document.createElement("div");
+		const cardInfo = document.createElement("div");
+		const title = document.createElement("h2");
+		const author = document.createElement("p");
+		const pages = document.createElement("p");
+		const readContainer = document.createElement("div");
+		const isReadIcon = document.createElement("img");
+		const noneReadIcon = document.createElement("img");
+
+		//ADD CLASSES TO ELEMENTS
+		card.classList.add("card");
+		cardInfo.classList.add("card-info-enable");
+		title.classList.add("card-header");
+		author.classList.add("card-author");
+		pages.classList.add("card-pages");
+		readContainer.classList.add("read-or-nonread-icons-container");
+		isReadIcon.classList.add("card-icon", "is-read");
+		noneReadIcon.classList.add("card-icon", "none-read");
+
+		//create data id to identify each card
+		card.setAttribute("data-id", book.id);
+
+		//Adding icon sources
+		isReadIcon.src = "assets/readcheck stats.png";
+		isReadIcon.alt = "";
+		noneReadIcon.src = "assets/cancel stats.png";
+		noneReadIcon.alt = "";
+
+		//Tex content from book object
+		title.textContent = book.title;
+		author.textContent = `By ${book.author}`;
+		pages.textContent = `${book.pages} pages`;
+
+		this.cardsContainer.appendChild(card);
+		card.appendChild(cardInfo);
+		cardInfo.appendChild(title);
+		cardInfo.appendChild(author);
+		cardInfo.appendChild(pages);
+		cardInfo.appendChild(readContainer);
+
+		const hoverCardBtns = this.createHoverBtns(
+			book,
+			card,
+			readContainer,
+			isReadIcon,
+			noneReadIcon
+		);
+		card.appendChild(hoverCardBtns);
+
+		card.addEventListener("mouseenter", () => {
+			hoverCardBtns.style.display = "block";
+			cardInfo.classList.add("blur-card");
+		});
+		card.addEventListener("mouseleave", () => {
+			hoverCardBtns.style.display = "none";
+			cardInfo.classList.remove("blur-card");
+		});
+
+		return card;
+	}
+
+	createHoverBtns(book, card, readContainer, isReadIcon, noneReadIcon) {
+		const cardHoverWrapper = document.createElement("div");
+		const cardWhenHover = document.createElement("div");
+		const haveReadContainer = document.createElement("div");
+		const toggleReadBtn = document.createElement("button");
+		const deleteContainer = document.createElement("div");
+		const deleteBtn = document.createElement("button");
+		const toggleReadImg = document.createElement("img");
+		const deleteImg = document.createElement("img");
+
+		cardHoverWrapper.classList.add("hover-wrapper");
+		haveReadContainer.classList.add("read-icons-container");
+		deleteContainer.classList.add("delete-icon-container");
+		cardWhenHover.classList.add("card-when-hover");
+		toggleReadBtn.classList.add("edit-btn-hover");
+		toggleReadImg.classList.add("read-img");
+		deleteBtn.classList.add("delete-btn-hover");
+
+		deleteImg.src = "assets/final-delete-btn-img.png";
+		deleteImg.alt = "trash icon";
+
+		toggleReadImg.src = book.isRead
+			? "assets/hover-read.png"
+			: "assets/hover-no-read.png";
+
+		cardHoverWrapper.appendChild(cardWhenHover);
+		cardWhenHover.appendChild(haveReadContainer);
+		cardWhenHover.appendChild(deleteContainer);
+		haveReadContainer.appendChild(toggleReadBtn);
+		toggleReadBtn.appendChild(toggleReadImg);
+		deleteContainer.appendChild(deleteBtn);
+		deleteBtn.appendChild(deleteImg);
+
+		cardHoverWrapper.style.display = "none";
+
+		toggleReadBtn.addEventListener("click", () =>
+			this.toggleReadStatus(
+				book,
+				toggleReadBtn,
+				readContainer,
+				isReadIcon,
+				noneReadIcon
+			)
+		);
+		deleteBtn.addEventListener("click", () => this.deleteBook(book, card));
+
+		return cardHoverWrapper;
+	}
+
+	toggleReadStatus(
 		book,
-		card,
+		toggleReadImg,
 		readContainer,
 		isReadIcon,
 		noneReadIcon
-	);
-	card.appendChild(btnsShowWhenHover);
+	) {
+		book.toggleRead();
+		toggleReadImg.src = book.isRead
+			? "assets/hover-read.png"
+			: "assets/hover-no-read.png";
 
-	//EVENT TO MAKE CARD HOVER DISPLAY THE BTN OF DELETE AND EDIT
-	card.addEventListener("mouseenter", () => {
-		btnsShowWhenHover.style.display = "block";
-		cardInfo.classList.add("blur-card");
-	});
-	card.addEventListener("mouseleave", () => {
-		btnsShowWhenHover.style.display = "none";
-		cardInfo.classList.remove("blur-card");
-	});
-
-	return card;
-}
-
-//FUNCTION TO CREATE THE CARD THAT APPEAR WHEN CARD IS HOVER
-
-function createHoverBtns(book, card, readContainer, isReadIcon, noneReadIcon) {
-	const cardHoverWrapper = document.createElement("div");
-	const cardWhenHover = document.createElement("div");
-	const haveReadContainer = document.createElement("div");
-	const toggleReadBtn = document.createElement("button");
-	const deleteContainer = document.createElement("div");
-	const deleteBtn = document.createElement("button");
-	const toggleReadImg = document.createElement("img");
-	const deleteImg = document.createElement("img");
-
-	cardHoverWrapper.classList.add("hover-wrapper");
-	haveReadContainer.classList.add("read-icons-container");
-	deleteContainer.classList.add("delete-icon-container");
-	cardWhenHover.classList.add("card-when-hover");
-	toggleReadBtn.classList.add("edit-btn-hover");
-	toggleReadImg.classList.add("read-img");
-	deleteBtn.classList.add("delete-btn-hover");
-
-	deleteImg.src = "assets/final-delete-btn-img.png";
-	deleteImg.alt = "trash icon";
-
-	if (book.isRead) {
-		toggleReadImg.src = "assets/hover-read.png";
-	} else {
-		toggleReadImg.src = "assets/hover-no-read.png";
-	}
-
-	cardHoverWrapper.appendChild(cardWhenHover);
-	cardWhenHover.appendChild(haveReadContainer);
-	cardWhenHover.appendChild(deleteContainer);
-	haveReadContainer.appendChild(toggleReadBtn);
-	toggleReadBtn.appendChild(toggleReadImg);
-	deleteContainer.appendChild(deleteBtn);
-	deleteBtn.appendChild(deleteImg);
-
-	cardHoverWrapper.style.display = "none";
-	toggleReadBtn.addEventListener("click", () => {
 		if (book.isRead) {
-			toggleReadImg.src = "assets/hover-no-read.png";
-			readContainer.appendChild(noneReadIcon);
-			readContainer.removeChild(isReadIcon);
-			book.isRead = false;
+			readContainer.replaceChild(isReadIcon, noneReadIcon);
 		} else {
-			toggleReadImg.src = "assets/hover-read.png";
-			readContainer.appendChild(isReadIcon);
-			readContainer.removeChild(noneReadIcon);
-			book.isRead = true;
+			readContainer.replaceChild(noneReadIcon, isReadIcon);
 		}
-		updateStatsWhenChaingReadStatus(book);
-		updateStatsDisplay();
-	});
 
-	deleteBtn.addEventListener("click", () => {
-		const cardId = parseInt(card.getAttribute("data-id"));
-		const bookToDelete = myLibrary.find((book) => book.id === cardId);
-		deleteCard(bookToDelete);
-	});
-
-	return cardHoverWrapper;
-}
-
-//delete book from library and update stats
-function deleteCard(book) {
-	//remove from dom
-	const cardToRemove = document.querySelector(`[data-id="${book.id}"]`);
-	cardToRemove.remove();
-
-	//remove from library array
-	const indexToDelete = myLibrary.findIndex((item) => item.id === book.id);
-	if (indexToDelete !== -1) {
-		myLibrary.splice(indexToDelete, 1);
-		saveLibraryInLocalStorage();
+		this.library.updateBook(book);
+		this.updateStatsDisplay();
 	}
-	//update stats
-	if (book.isRead) {
-		readCount--;
-		pagesCount -= parseInt(book.pages);
-	} else {
-		noReadCount--;
+
+	deleteBook(book, card) {
+		this.library.removeBook(book.id);
+		card.remove();
+		this.updateStatsDisplay();
 	}
-	TotalbookCount--;
-	updateStatsDisplay();
-}
-btnSubmitBook.addEventListener("click", addNewBook, createBookCard);
 
-let readCount = 0;
-let noReadCount = 0;
-let pagesCount = 0;
-let TotalbookCount = 0;
+	updateStatsDisplay() {
+		const bookStatsDisplay = document.querySelector(".stack-count");
+		const bookPageStatsDisplay = document.querySelector(".pages-count");
+		const booksReadDisplay = document.querySelector(".read-count");
+		const booksNotReadDisplay = document.querySelector(".non-read-count");
 
-//where it will be displayed
-function updateStatsDisplay() {
-	const bookStatsDisplay = document.querySelector(".stack-count");
-	const bookPageStatsDisplay = document.querySelector(".pages-count");
-	const booksReadDisplay = document.querySelector(".read-count");
-	const booksNotReadDisplay = document.querySelector(".non-read-count");
-	bookStatsDisplay.textContent = TotalbookCount;
-	bookPageStatsDisplay.textContent = pagesCount;
-	booksReadDisplay.textContent = readCount;
-	booksNotReadDisplay.textContent = noReadCount;
-}
-
-// Function to update statistics display when a book card is created
-function updateStatsWhenCardCreated(book) {
-	if (book.isRead) {
-		readCount++;
-		pagesCount += parseInt(book.pages);
-	} else {
-		noReadCount++;
+		bookStatsDisplay.textContent = this.library.getTotalBooks();
+		bookPageStatsDisplay.textContent = this.library.getTotalPagesRead();
+		booksReadDisplay.textContent = this.library.getReadBooks();
+		booksNotReadDisplay.textContent = this.library.getUnreadBooks();
 	}
-	TotalbookCount++;
-	updateStatsDisplay();
-}
 
-function ensureNonNegative(value) {
-	return Math.max(0, value);
-}
+	addNewBook() {
+		const title = this.titleInput.value;
+		const author = this.authorInput.value;
+		const pages = this.pagesInput.value;
+		const isRead = this.readOrNotInput.checked;
 
-// Function to update statistics when changing read status of a book
-function updateStatsWhenChaingReadStatus(book) {
-	if (book.isRead) {
-		readCount++;
-		noReadCount--;
-		pagesCount += parseInt(book.pages);
-	} else {
-		readCount--;
-		noReadCount++;
-		pagesCount -= parseInt(book.pages);
+		const newBook = new Book(title, author, pages, isRead);
+		this.library.addBook(newBook);
+		this.createBookCard(newBook);
+		this.updateStatsDisplay();
+		this.closeModal();
+		this.clearInputFields();
 	}
+
+	openModal() {
+		this.modalToAddBook.style.display = "block";
+		this.wrapper.classList.add("blur-bcg");
+	}
+
+	closeModal() {
+		this.modalToAddBook.style.display = "none";
+		this.wrapper.classList.remove("blur-bcg");
+	}
+
+	clearInputFields() {
+		this.titleInput.value = "";
+		this.authorInput.value = "";
+		this.pagesInput.value = "";
+		this.readOrNotInput.checked = false;
+	}
+
+    displayAllBooks(){
+        this.cardsContainer.innerHTML = ""; // Clear existing cards
+		this.library.getBooks().forEach((book) => this.createBookCard(book));
+		this.updateStatsDisplay();
+    }
 }
 
-//load library from local storage
-window.addEventListener("load", loadLibraryFromLocalStorage);
+const library = new Library();
+const ui = new Ui(library);
+
+window.addEventListener("load", () => ui.init());
